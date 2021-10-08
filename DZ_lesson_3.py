358cdea1e7bcee2d8e42e0b1c0bf29ec5f46d7dc
KirilUdovenko <kiril.stuff1@gmail.com>
2021-10-09 01:00:39 +0300

    DZ_lesson_3 v.1

---
+++ DZ_lesson_3.py
@@ -0,0 +1,36 @@
+################
+
+# my_list = [15, 25, 75, 105, 125, 205, 175, 34, 132]
+# for symbol in my_list:
+#     if symbol > 100:
+#         print(symbol)
+
+##################
+
+# my_list = [15, 25, 75, 105, 125, 205, 175, 34, 132]
+# my_results = []
+# for symbol in my_list:
+#     if symbol > 100:
+#         my_result.append(symbol)
+# print(my_results)
+
+#####################
+
+# my_list = [7, 14, 45]
+# if len(my_list) < 2:
+#     my_list.append(0)
+#     print(my_list)
+# else:
+#     print(my_list[-1] + my_list[-2])
+
+#######################
+
+# my_string = '0123456789'
+# my_list = []
+# my_string_1 = "0123456789"
+# my_string_2 = "0123456789"
+# for symbol_1 in my_string_1:
+#     for symbol_2 in my_string_2:
+#         result = symbol_1 + symbol_2
+#         my_list.append(result)
+# print(my_list)
