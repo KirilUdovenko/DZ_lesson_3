77fed4e96b844062d0be18bb61c34d43d91dbee6
Kereeshkaa <kiril.suff1@gmail.com>
2021-10-09 18:57:14 +0300

    fixed DZ 3

--- DZ_lesson_3.py
+++ DZ_lesson_3.py
@@ -16,12 +16,12 @@
 
 #####################
 
-# my_list = [7, 14, 45]
+# my_list = [7]
 # if len(my_list) < 2:
 #     my_list.append(0)
-#     print(my_list)
 # else:
-#     print(my_list[-1] + my_list[-2])
+#     my_list.append(my_list[-1] + my_list[-2])
+# print(my_list)
 
 #######################
 
@@ -31,6 +31,6 @@
 # my_string_2 = "0123456789"
 # for symbol_1 in my_string_1:
 #     for symbol_2 in my_string_2:
-#         result = symbol_1 + symbol_2
-#         my_list.append(result)
+#         result = int(symbol_1 + symbol_2)
+#         my_list.append(int(result))
 # print(my_list)
